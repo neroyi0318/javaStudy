9a8ce87c197fd63686a1328bac38fcab0418d547
DESKTOP-NUOPUIF\ITPS <yichangsin@gmail.com>
2021-03-15 16:56:10 +0900

    프로젝트 최초 업로드

---
+++ src/chapter1/Ex2.java
@@ -0,0 +1,20 @@
+package chapter1;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		// 문자열 - 문자의 나열
+		// 문자열은 꼭 쌍따옴표를 사용해서 표현해야한다.
+//		System.out.println("Hello World~!");
+		
+//		System.out.println("Hello");
+//		System.out.println("World~!");
+		
+		System.out.println("I am");
+		System.out.println("a");
+		System.out.println("Java Programmer");
+		
+//		System.out.print("오늘은 ");
+//		System.out.print("자바 수업 ");
+//		System.out.print("첫 번째 날입니다.");
+	}
+}
