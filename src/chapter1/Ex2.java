724345007b8da0959e542692031e1c160bd10bdb
DESKTOP-NUOPUIF\ITPS <yichangsin@gmail.com>
2021-03-15 17:19:05 +0900

    최초 프로젝트 업로드

--- src/chapter1/Ex2.java
+++ src/chapter1/Ex2.java
@@ -9,12 +9,21 @@ public class Ex2 {
 //		System.out.println("Hello");
 //		System.out.println("World~!");
 		
-		System.out.println("I am");
-		System.out.println("a");
-		System.out.println("Java Programmer");
+//		System.out.println("I am");
+//		System.out.println("a");
+//		System.out.println("Java Programmer");
 		
 //		System.out.print("오늘은 ");
 //		System.out.print("자바 수업 ");
 //		System.out.print("첫 번째 날입니다.");
+		
+		// 정수
+		System.out.println(1);
+		// 실수
+		System.out.println(3.14);
+		// 문자
+		System.out.println('a');
+		// 문자열
+		System.out.println("abcd");
 	}
 }
